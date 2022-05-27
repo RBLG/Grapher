9aee6d247fb60c94ed9c63b743f5a32bd87deae3
RBLG <37543249+RBLG@users.noreply.github.com>
2022-05-27 20:04:29 +0200

    stuff

--- Graph3DEditor.Designer.cs
+++ Graph3DEditor.Designer.cs
@@ -28,19 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.canvas3D1 = new Grapher.Canvas3D();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.brushSize = new System.Windows.Forms.TrackBar();
+            this.canvas3D1 = new Grapher.Canvas3D();
+            this.BrushThing2 = new System.Windows.Forms.TrackBar();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.brushSize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BrushThing2)).BeginInit();
             this.SuspendLayout();
-            // 
-            // canvas3D1
-            // 
-            this.canvas3D1.Location = new System.Drawing.Point(84, 6);
-            this.canvas3D1.Name = "canvas3D1";
-            this.canvas3D1.Size = new System.Drawing.Size(758, 511);
-            this.canvas3D1.TabIndex = 1;
-            this.canvas3D1.Text = "canvas3D1";
             // 
             // comboBox1
             // 
@@ -66,17 +69,116 @@
             this.comboBox3.Size = new System.Drawing.Size(75, 21);
             this.comboBox3.TabIndex = 4;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Brush :";
+            // 
+            // brushSize
+            // 
+            this.brushSize.Location = new System.Drawing.Point(6, 117);
+            this.brushSize.Name = "brushSize";
+            this.brushSize.Size = new System.Drawing.Size(71, 45);
+            this.brushSize.TabIndex = 6;
+            this.brushSize.TickStyle = System.Windows.Forms.TickStyle.None;
+            // 
+            // canvas3D1
+            // 
+            this.canvas3D1.Location = new System.Drawing.Point(84, 6);
+            this.canvas3D1.Name = "canvas3D1";
+            this.canvas3D1.Size = new System.Drawing.Size(758, 511);
+            this.canvas3D1.TabIndex = 1;
+            this.canvas3D1.Text = "canvas3D1";
+            // 
+            // BrushThing2
+            // 
+            this.BrushThing2.Location = new System.Drawing.Point(6, 168);
+            this.BrushThing2.Name = "BrushThing2";
+            this.BrushThing2.Size = new System.Drawing.Size(71, 45);
+            this.BrushThing2.TabIndex = 7;
+            this.BrushThing2.TickStyle = System.Windows.Forms.TickStyle.None;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(6, 219);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(27, 219);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(15, 14);
+            this.checkBox2.TabIndex = 9;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(48, 219);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(15, 14);
+            this.checkBox3.TabIndex = 10;
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(6, 239);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(15, 14);
+            this.checkBox4.TabIndex = 11;
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(27, 239);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(15, 14);
+            this.checkBox5.TabIndex = 12;
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(48, 239);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(15, 14);
+            this.checkBox6.TabIndex = 13;
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
             // Graph3DEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.BrushThing2);
+            this.Controls.Add(this.brushSize);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBox3);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.canvas3D1);
             this.Name = "Graph3DEditor";
             this.Size = new System.Drawing.Size(845, 517);
+            ((System.ComponentModel.ISupportInitialize)(this.brushSize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BrushThing2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -85,5 +187,14 @@
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar brushSize;
+        private System.Windows.Forms.TrackBar BrushThing2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
     }
 }
