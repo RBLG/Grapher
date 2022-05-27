5c6ab4164b35fb519ffbe5cec7473bf683ec7241
RBLG <37543249+RBLG@users.noreply.github.com>
2022-05-27 18:03:30 +0200

    add submodule naudio

--- Graph3DEditor.Designer.cs
+++ Graph3DEditor.Designer.cs
@@ -28,33 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.canvas3D1 = new Grapher.Canvas3D();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(3, 3);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // canvas3D1
             // 
-            this.canvas3D1.Location = new System.Drawing.Point(84, 3);
+            this.canvas3D1.Location = new System.Drawing.Point(84, 6);
             this.canvas3D1.Name = "canvas3D1";
             this.canvas3D1.Size = new System.Drawing.Size(758, 511);
             this.canvas3D1.TabIndex = 1;
             this.canvas3D1.Text = "canvas3D1";
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(3, 30);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(75, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(3, 57);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(75, 21);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(3, 3);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(75, 21);
+            this.comboBox3.TabIndex = 4;
+            // 
             // Graph3DEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.canvas3D1);
-            this.Controls.Add(this.button1);
             this.Name = "Graph3DEditor";
             this.Size = new System.Drawing.Size(845, 517);
             this.ResumeLayout(false);
@@ -62,8 +81,9 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private Canvas3D canvas3D1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
     }
 }
