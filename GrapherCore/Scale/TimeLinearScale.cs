945484501e18ad9528c92725fe7cf78a444611a6
RBLG <37543249+RBLG@users.noreply.github.com>
2023-05-15 20:39:56 +0200

    upd time linear scale + compiler settings

--- GrapherCore/Scale/TimeLinearScale.cs
+++ GrapherCore/Scale/TimeLinearScale.cs
@@ -43,13 +43,16 @@ namespace Grapher.Scale
             }
             else
             {
+                //need a better way to handle when release happen before time reached hold
+                // -> get closest pre hold index after release, reach it then interpolate on the next
+
                 if (double.IsNaN(spectrum.TimeOff))
                 { rtn = Math.Min(rtn, Hold); }
                 else
                 {
                     //if release event happenned before time reached the hold, use time instead of timeoff
                     rtn = Math.Min(spectrum.Time, Hold + spectrum.TimeOff);
-                    rtn = Math.Min(rtn, Duration - 1); //
+                    rtn = Math.Min(rtn, Duration - 1); //-1 because weird stuff happen at duration value, will check later
                 }
                 rtn /= Duration;
             }
@@ -58,7 +61,53 @@ namespace Grapher.Scale
 
         public (int, int, double) PickValueTo2(Wave wave, Spectrum spectrum, int size)
         {
-            return Table.PrepareInterpolation(PickValueTo(wave, spectrum, size), size, IsLooping);
+
+            double rtn = spectrum.Time;
+            if (IsLooping)
+            {
+                rtn /= Duration;
+                rtn -= (int)rtn;//%= 1;//another faster modulo
+                rtn *= size;
+                return Table.PrepareInterpolation(rtn, size, IsLooping);
+            }
+            else
+            {
+                //need a better way to handle when release happen before time reached hold
+                // -> get closest pre hold index after release, reach it then interpolate on the next
+
+                if (double.IsNaN(spectrum.TimeOff))
+                {
+                    rtn = Math.Min(rtn, Hold);
+                    rtn /= Duration;
+                    rtn *= size;
+                    return Table.PrepareInterpolation(rtn, size, IsLooping);
+                }
+                else
+                {
+                    double release = spectrum.Time - spectrum.TimeOff;//remove calculations by having release time in wave?
+                    if (release < Hold)
+                    {
+                        double realease = release - release % (Duration / size);
+                        if (spectrum.Time < realease)
+                        {
+                            double time1 = spectrum.Time * size / Duration;
+                            int index1 = (int)time1;
+                            int index2 = (int)((Hold + spectrum.TimeOff) * size / Duration);
+                            double mix = time1 - index1;
+
+                            return (index1, index2, mix);
+                        }
+                    }
+
+                    rtn = Hold + spectrum.TimeOff;
+
+                    rtn = Math.Min(rtn, Duration - 1); //-1 because weird stuff happen at duration value, will check later
+                    rtn /= Duration;
+                    rtn *= size;
+                    return Table.PrepareInterpolation(rtn, size, IsLooping);
+                }
+
+            }
         }
 
     }
